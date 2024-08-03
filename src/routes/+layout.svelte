e138c174c48e65b3bb6b66e71dd29ba6c1df2b65
untemi <94703538+andro404-MC@users.noreply.github.com>
2024-08-03 22:50:41 +0100

    ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import "../app.css";
+  import Icon from '@iconify/svelte';
+</script>
+
+<div class="bg-base-200 min-h-screen">
+  <div class="navbar sticky top-0 z-50 bg-base-100 shadow-lg">
+    <div class="flex-1">
+      <a href="/" class="btn btn-ghost text-xl">Agrou Shop</a>
+    </div>
+
+    <div class="flex-none">
+      <a href="/shop" class="btn btn-square btn-ghost">
+        <Icon width="1.4rem" icon="ic:outline-shopping-cart"/>
+      </a>
+      <a href="/about" class="btn btn-square btn-ghost">
+        <Icon width="1.4rem" icon="ic:outline-info"/>
+      </a>
+    </div>
+  </div>
+
+  <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+  class="hero min-h-screen"
+  style="background-image: url(https://untemi.sirv.com/agrou/agroubg.webp);">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Bonjour!</h1>
+      <p class="mb-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores blanditiis illo officia molestiae         magnam id, laborum fugiat sunt ab quae aut dicta, non aliquam consectetur, natus molestias. Quam, vero aliquam.
+      </p>
+      <a href="/shop" class="btn btn-primary">Shop</a>
+    </div>
+  </div>
+</div>
