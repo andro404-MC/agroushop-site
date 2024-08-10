61c4245c3887196dbd0cde1ecfb3b330f9c4336c
untemi <94703538+andro404-MC@users.noreply.github.com>
2024-08-10 12:36:32 +0100

    aiven

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,5 @@
 import postgres from "postgres";
 
 const sql = postgres(process.env.PQADRR);
-await sql`CREATE TABLE IF NOT EXISTS article (
-      id SERIAL PRIMARY KEY,
-      name TEXT,
-      price REAL,
-      stock INTEGER,
-      category TEXT,
-      picurl TEXT,
-      discrate INTEGER,
-      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-    )`;
 
 export default sql;
